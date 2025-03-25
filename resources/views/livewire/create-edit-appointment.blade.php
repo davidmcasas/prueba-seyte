083fa2f41bcbb7a1ea9e11888d71ecc88d50ed07
davidmcasas <info@davidmcasas.com>
2025-03-25 21:39:50 +0100

    Fix Alerts

--- resources/views/livewire/create-edit-appointment.blade.php
+++ resources/views/livewire/create-edit-appointment.blade.php
@@ -1,14 +1,20 @@
 <div>
     @if (session()->has('message'))
-        <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)"
+        <div x-data="{ show: true }"
+             x-show="show"
+             x-init="setTimeout(() => show = false, 3000)"
+             x-transition.opacity
              class="fixed bottom-10 right-10 bg-green-500 text-white p-4 rounded-lg shadow-md z-50">
             {{ session('message') }}
         </div>
     @endif
     @if (session()->has('error'))
-        <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)"
+        <div x-data="{ show: true }"
+             x-show="show"
+             x-init="setTimeout(() => show = false, 3000)"
+             x-transition.opacity
              class="fixed bottom-10 right-10 bg-red-500 text-white p-4 rounded-lg shadow-md z-50">
-            {{ session('message') }}
+            {{ session('error') }}
         </div>
     @endif
     @if($isOpen)
