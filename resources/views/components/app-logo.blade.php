712c666e196bf11e332d19ab6139a8bda5595aa7
davidmcasas <info@davidmcasas.com>
2025-03-26 00:03:43 +0100

    Corregir texto logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,6 +2,6 @@
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Gestión de Citas</span>
+    <span class="mb-0.5 text-white truncate leading-none font-semibold">Gestión de Citas</span>
     <span class="mb-0.5 text-gray-400 truncate leading-none font-semibold">(Prueba Seyte)</span>
 </div>
