2c2723e2e5f0b95fd216d801f3c4455ea0b0c40a
davidmcasas <info@davidmcasas.com>
2025-03-26 08:25:21 +0100

    Regenerar API Token en session expirada

---
+++ app/Http/Middleware/EnsureApiTokenInSession.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureApiTokenInSession
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && !Session::has('auth_token')) {
+            $token = Auth::user()->createToken('auth_token')->plainTextToken;
+            Session::put('auth_token', $token);
+        }
+
+        return $next($request);
+    }
+}
